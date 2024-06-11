dcb4aebb1dd1e0632da7a77df92008794a45df3c
boaloysius <boaloysius@gmail.com>
2024-06-12 00:11:57 +0530

    Add shadcn vue for components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <div class="bg-red-900">Hi there, welcome to rails app</div>
+  <div>
+    <div class="bg-red-900">Hi there, welcome to rails app</div>
+    <Button>Button</Button>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Button } from "@/components/ui/button";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Button,
+  },
+});
+</script>
